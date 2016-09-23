6f4bd02e99d55d79f6504144c9c7a189f2dcd289
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2016-09-23 17:45:23 +0300

    Remove multicheck control

--- inc/customizer-controls.php
+++ inc/customizer-controls.php
@@ -71,68 +71,6 @@ class Counter_Message_Control extends WP_Customize_Control {
 }
 
 /**
- * Multiple checkbox customize control class.
- *
- * @access public
- */
-class Counter_Multicheck_Control extends WP_Customize_Control {
-
-	/**
-	 * The type of customize control being rendered.
-	 *
-	 * @access public
-	 * @var    string
-	 */
-	public $type = 'multicheck';
-
-	/**
-	 * Displays the control content.
-	 *
-	 * @access public
-	 * @return void
-	 */
-	public function render_content() {
-
-		if ( empty( $this->choices ) ) {
-			return;
-		}
-
-		// Title.
-		if ( ! empty( $this->label ) ) {
-			printf(
-				'<span class="customize-control-title">%s</span>',
-				esc_html( $this->label )
-			);
-		}
-
-		// Description.
-		if ( ! empty( $this->description ) ) {
-			printf(
-				'<p class="customize-control-paragraph">%s</p>',
-				wp_kses_post( $this->description )
-			);
-		}
-
-		$values = ! is_array( $this->value() ) ? explode( ',', $this->value() ) : $this->value(); ?>
-
-		<ul>
-			<?php foreach ( $this->choices as $value => $label ) : ?>
-
-				<li>
-					<label>
-						<input type="checkbox" value="<?php echo esc_attr( $value ); ?>" <?php checked( in_array( $value, $values ) ); ?> />
-						<?php echo esc_html( $label ); ?>
-					</label>
-				</li>
-
-			<?php endforeach; ?>
-		</ul>
-
-		<input type="hidden" <?php $this->link(); ?> value="<?php echo esc_attr( implode( ',', $values ) ); ?>" /> <?php
-	}
-}
-
-/**
  * Displays divider.
  */
 class Counter_Divider_Control extends WP_Customize_Control {
