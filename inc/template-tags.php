27fe76911d205039883bac5df89bf4d413e00094
Dmitry <iamdmitrymayorov@gmail.com>
2017-01-18 13:29:36 +0500

    Update template-tags.php

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -5,17 +5,6 @@
  * @package Counter
  */
 
-/**
- * Displays custom logo for the site.
- *
- * @todo Remove when 4.7 is out
- */
-function counter_site_logo() {
-	if ( function_exists( 'the_custom_logo' ) ) {
-		the_custom_logo();
-	}
-}
-
 if ( ! function_exists( 'counter_entry_meta_header' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author.
@@ -35,9 +24,9 @@ function counter_entry_meta_header() {
 	 *
 	 * Translators: used between list items, there is a space after the comma.
 	 */
-	$categories_list = get_the_category_list( esc_html__( ', ', 'counter' ) );
-	if ( $categories_list && counter_categorized_blog() && in_array( 'cat-links', $meta_items ) || $is_customizer ) {
-		printf( '<span class="entry-meta-item cat-links">' . esc_html__( '%1$s', 'counter' ) . '</span>', $categories_list ); // WPCS: XSS OK.
+	$categories_list = get_the_category_list( ', ' );
+	if ( $categories_list && counter_categorized_blog() ) {
+		printf( '<div class="entry-meta-item cat-links">%s</div>', $categories_list ); // WPCS: XSS OK.
 	}
 
 	/*
@@ -99,43 +88,37 @@ function counter_entry_meta_footer() {
 	// Hide category and tag text for pages.
 	if ( 'post' == get_post_type() ) {
 		/* translators: used between list items, there is a space after the comma */
-		$tags_list = get_the_tag_list( '', esc_html__( '', 'counter' ) );
+		$tags_list = get_the_tag_list( '', '' );
 		if ( $tags_list ) {
-			printf( '<footer class="entry-footer"><span class="tags-links">' . esc_html__( '%1$s', 'counter' ) . '</span></footer><!-- .entry-footer -->', $tags_list ); // WPCS: XSS OK.
+			printf( '<footer class="entry-footer"><span class="tags-links">%s</span></footer><!-- .entry-footer -->', $tags_list ); // WPCS: XSS OK.
 		}
 	}
 }
 endif;
 
-if ( ! function_exists( 'counter_site_title' ) ) :
+if ( ! function_exists( 'counter_site_title_tagline' ) ) :
 /**
- * Displays site title.
+ * Displays site title and tagline.
  */
-function counter_site_title() {
-	$class = 'site-title';
-	// Set the second argument to 'true' to display title by default.
-	if ( ! get_theme_mod( 'display_title', true ) ) {
-		$class .= ' screen-reader-text';
-	}
-	printf( '<h1 class="%s"><a href="%s" rel="home">%s</a></h1>',
-		esc_attr( $class ),
-		esc_url( home_url( '/' ) ),
-		esc_html( get_bloginfo( 'name' ) )
-	);
-}
-endif;
+function counter_site_title_tagline() {
+	$title = get_bloginfo( 'name' );
+	$tagline = get_bloginfo( 'description', 'display' );
 
-if ( ! function_exists( 'counter_site_tagline' ) ) :
-/**
- * Displays site description.
- */
-function counter_site_tagline() {
-	$class = 'site-description';
-	// Set the second argument to 'true' to display tagline by default.
-	if ( ! get_theme_mod( 'display_tagline', true ) ) {
-		$class .= ' screen-reader-text';
+	if ( $title || is_customize_preview() ) {
+		printf(
+			'<%1$s class="site-title"><a href="%2$s">%3$s</a></%1$s>',
+			is_front_page() ? 'h1' : 'p',
+			esc_url( home_url( '/' ) ),
+			esc_html( $title )
+		);
 	}
-	printf( '<h2 class="%s">%s</h2>', esc_attr( $class ), esc_html( get_bloginfo( 'description' ) ) );
+
+	if ( $tagline || is_customize_preview() ) {
+		printf(
+			'<p class="site-description">%s</p>',
+			esc_html( $tagline )
+		);
+	}
 }
 endif;
 
@@ -153,11 +136,11 @@ function counter_post_thumbnail() {
 
 	if ( is_singular( array( 'post', 'page' ) ) ) {
 		echo '<div class="post-thumbnail">';
-			the_post_thumbnail( 'counter-thumbnail-single' );
+			the_post_thumbnail( '936x0' );
 		echo '</div>';
 	} else {
 		printf( '<a class="post-thumbnail" href="%s">', esc_url( apply_filters( 'the_permalink', get_permalink() ) ) );
-			the_post_thumbnail( 'counter-thumbnail' );
+			the_post_thumbnail( '612x0' );
 		echo '</a>';
 	}
 }
@@ -217,7 +200,6 @@ function counter_comment_navigation() {
 }
 endif;
 
-if ( ! function_exists( 'counter_footer_text' ) ) :
 /**
  * Displays Footer Text.
  */
@@ -228,7 +210,6 @@ function counter_footer_text() {
 		'<a href="' . esc_url( 'https://themepatio.com/' ) . '">ThemePatio</a>'
 	);
 }
-endif;
 
 /**
  * Returns true if a blog has more than 1 category.
@@ -273,21 +254,6 @@ add_action( 'edit_category', 'counter_category_transient_flusher' );
 add_action( 'save_post',     'counter_category_transient_flusher' );
 
 /**
- * Panels on the front page.
- */
-function counter_panels() {
-	for ( $i = 1; $i <= 10; $i++ ) :
-		if ( get_theme_mod( 'panel_' . $i ) ) :
-			$post = get_post( get_theme_mod( 'panel_' . $i ) );
-			setup_postdata( $post );
-			set_query_var( 'counter_panel', $i );
-			get_template_part( 'template-parts/content', 'front' );
-			wp_reset_postdata();
-		endif;
-	endfor;
-}
-
-/**
  * Displays panel thumbnail.
  */
 function counter_panel_thumbnail() {
@@ -296,7 +262,7 @@ function counter_panel_thumbnail() {
 	}
 	if ( has_post_thumbnail() ) {
 		echo '<div class="panel-thumbnail">';
-			the_post_thumbnail( 'counter-panel-half' );
+			the_post_thumbnail( '720x0' );
 		echo '</div>';
 	} else {
 		?>
@@ -311,31 +277,9 @@ function counter_panel_thumbnail() {
 }
 
 /**
- * Displays panel title.
- *
- * @param int $num The number of the panel.
- */
-function counter_panel_title( $num ) {
-	// Check if we need to display title.
-	if ( is_customize_preview() ) {
-		if ( get_theme_mod( 'panel_title_display_' . $num, 1 ) ) {
-			the_title( '<h2 class="panel-title">', '</h2>' );
-		} else {
-			the_title( '<h2 class="panel-title" style="display: none;">', '</h2>' );
-		}
-	} elseif ( get_theme_mod( 'panel_title_display_' . $num, 1 )  ) {
-		// Output heading with the screen reader class or not.
-		the_title( '<h2 class="panel-title">', '</h2>' );
-	}
-}
-
-/**
  * Displays panel content.
- *
- * @param int $num The number of the panel.
  */
-function counter_panel_content( $num ) {
-	// Check if we need to display the content first.
+function counter_panel_content() {
 	if ( get_the_content() ) { ?>
 		<div class="panel-content">
 			<?php the_content( __( 'Read more', 'counter' ) ); ?>
